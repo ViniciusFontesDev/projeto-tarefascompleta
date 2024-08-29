653d8c8f2f30fbe133a6694392cebfcedd1627c9
Vinícius Fontes <programadorviniciusfontes@gmail.com>
2024-08-29 11:11:51 -0400

    Projeto Finalizado

---
+++ script.js
@@ -0,0 +1,51 @@
+let inputElement = document.querySelector("#place input");
+let buttonElement = document.querySelector("#place button");
+let listElement = document.querySelector("#place ul");
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
+
+function renderTarefas(){
+    listElement.innerHTML = "";
+    tarefas.map((todo) => {
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(todo);
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+        let linkText = document.createTextNode("Excluir");
+        linkElement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(todo);
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+
+        liElement.appendChild(tarefaText);
+        liElement.appendChild(linkElement);
+        listElement.appendChild(liElement);
+    })
+}
+
+renderTarefas();
+
+function adicionarTarefas(){
+    if(inputElement.value === ''){
+        alert("Tarefa não adicionada");
+    }else{
+        let novaTarefa = inputElement.value;
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+
+        renderTarefas();
+        salvarDados();
+    }   
+}
+
+buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1);
+
+    renderTarefas();
+    salvarDados();
+}
+
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas));
+}
